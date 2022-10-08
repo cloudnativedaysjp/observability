61f551ebe8bf3c3994eb0828eec988d21f5f3c40
Hiroki Okui <hiroki.okui@gmail.com>
2022-10-08 21:58:46 +0900

    feat: Use fire to generate CLI

--- people-counter-demo/main.py
+++ people-counter-demo/main.py
@@ -1,19 +1,11 @@
 #!/usr/bin/env python3
-from pathlib import Path
-
 import blobconverter
 import cv2
 import depthai as dai
-import numpy as np
-import argparse
-from time import monotonic
-import itertools
+import fire
 
 from depthai_sdk import PipelineManager, NNetManager, PreviewManager
-from depthai_sdk import cropToAspectRatio
 from prometheus_client import CollectorRegistry, Gauge, push_to_gateway
-
-parentDir = Path(__file__).parent
 
 #=====================================================================================
 # To use a different NN, change `size` and `nnPath` here:
@@ -24,60 +16,62 @@ nnPath = blobconverter.from_zoo("person-detection-retail-0013", shaves=8)
 # Labels
 labelMap = ["background", "person"]
 
-# Get argument first
-parser = argparse.ArgumentParser()
-parser.add_argument('-d', '--debug', action="store_true",
-                    help="Debug using camera view")
-args = parser.parse_args()
-
 # Whether we want to use images from host or rgb camera
 nnSource = "color"
 
-def main():
 
-    # Start defining a pipeline
-    pm = PipelineManager()
-    pm.createColorCam(previewSize=size, xout=True)
-    pv = PreviewManager(display=["color"], nnSource=nnSource)
+class Command:
 
-    nm = NNetManager(inputSize=size, nnFamily="mobilenet", labels=labelMap, confidence=0.5)
-    nn = nm.createNN(pm.pipeline, pm.nodes, blobPath=nnPath, source=nnSource)
-    pm.setNnManager(nm)
-    pm.addNn(nn)
+    def people_count(
+            self,
+            push_gateway_addr: str = '',
+            debug: bool = False
+    ):
+        # Start defining a pipeline
+        pm = PipelineManager()
+        pm.createColorCam(previewSize=size, xout=True)
+        pv = PreviewManager(display=["color"], nnSource=nnSource)
 
-    # Pipeline defined, now the device is connected to
-    with dai.Device(pm.pipeline) as device:
-        nm.createQueues(device)
-        pv.createQueues(device)
+        nm = NNetManager(inputSize=size, nnFamily="mobilenet", labels=labelMap, confidence=0.5)
+        nn = nm.createNN(pm.pipeline, pm.nodes, blobPath=nnPath, source=nnSource)
+        pm.setNnManager(nm)
+        pm.addNn(nn)
 
-        count = 0
-        while True:
-            count += 1
-            pv.prepareFrames(blocking=True)
-            frame = pv.get("color")
+        # Pipeline defined, now the device is connected to
+        with dai.Device(pm.pipeline) as device:
+            nm.createQueues(device)
+            pv.createQueues(device)
 
-            nn_data = nm.decode(nm.outputQueue.get())
-            nm.draw(frame, nn_data)
-            print(len(nn_data))
+            count = 0
+            while True:
+                count += 1
+                pv.prepareFrames(blocking=True)
 
-            if args.debug:
-                cv2.putText(frame, f"People count: {len(nn_data)}", (5, 30), cv2.FONT_HERSHEY_TRIPLEX, 1, (0,0,255))
-                cv2.imshow("color", frame)
+                nn_data = nm.decode(nm.outputQueue.get())
+                print(len(nn_data))
 
-            if cv2.waitKey(1) == ord('q'):
-                break
+                if debug:
+                    frame = pv.get("color")
+                    nm.draw(frame, nn_data)
+                    cv2.putText(frame, f"People count: {len(nn_data)}", (5, 30), cv2.FONT_HERSHEY_TRIPLEX, 1,
+                                (0, 0, 255))
+                    cv2.imshow("color", frame)
 
-            if count > 10:
-                count = 0
-                send_count(len(nn_data))
+                    if cv2.waitKey(1) == ord('q'):
+                        break
+
+                if count > 10:
+                    count = 0
+                    if push_gateway_addr:
+                        send_count(len(nn_data), push_gateway_addr)
 
 
-def send_count(count: int):
+def send_count(count: int, addr: str):
     registry = CollectorRegistry()
     g = Gauge('people_count', 'People count', registry=registry)
     g.set(count)
-    push_to_gateway('localhost:9091', job='depthai', registry=registry)
+    push_to_gateway(addr, job='depthai', registry=registry)
 
 
 if __name__ == "__main__":
-    main()
+    fire.Fire(Command)
