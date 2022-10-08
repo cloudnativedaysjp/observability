4ce55578a07203b1c14c4f0aedbf71e6d3658681
Hiroki Okui <hiroki.okui@gmail.com>
2022-10-09 00:53:34 +0900

    refactoring: Extract depthai manager class

--- people-counter-demo/main.py
+++ people-counter-demo/main.py
@@ -1,24 +1,9 @@
 #!/usr/bin/env python3
-import blobconverter
-import cv2
-import depthai as dai
 import fire
 
-from depthai_sdk import PipelineManager, NNetManager, PreviewManager
 from prometheus_client import CollectorRegistry, Gauge, push_to_gateway
 
-#=====================================================================================
-# To use a different NN, change `size` and `nnPath` here:
-size = (544, 320)
-nnPath = blobconverter.from_zoo("person-detection-retail-0013", shaves=8)
-#=====================================================================================
-
-# Labels
-labelMap = ["background", "person"]
-
-# Whether we want to use images from host or rgb camera
-nnSource = "color"
-
+from src.depthai import DepthAi
 
 class Command:
 
@@ -27,43 +12,8 @@ class Command:
             push_gateway_addr: str = '',
             debug: bool = False
     ):
-        # Start defining a pipeline
-        pm = PipelineManager()
-        pm.createColorCam(previewSize=size, xout=True)
-        pv = PreviewManager(display=["color"], nnSource=nnSource)
-
-        nm = NNetManager(inputSize=size, nnFamily="mobilenet", labels=labelMap, confidence=0.5)
-        nn = nm.createNN(pm.pipeline, pm.nodes, blobPath=nnPath, source=nnSource)
-        pm.setNnManager(nm)
-        pm.addNn(nn)
-
-        # Pipeline defined, now the device is connected to
-        with dai.Device(pm.pipeline) as device:
-            nm.createQueues(device)
-            pv.createQueues(device)
-
-            count = 0
-            while True:
-                count += 1
-                pv.prepareFrames(blocking=True)
-
-                nn_data = nm.decode(nm.outputQueue.get())
-                print(len(nn_data))
-
-                if debug:
-                    frame = pv.get("color")
-                    nm.draw(frame, nn_data)
-                    cv2.putText(frame, f"People count: {len(nn_data)}", (5, 30), cv2.FONT_HERSHEY_TRIPLEX, 1,
-                                (0, 0, 255))
-                    cv2.imshow("color", frame)
-
-                    if cv2.waitKey(1) == ord('q'):
-                        break
-
-                if count > 10:
-                    count = 0
-                    if push_gateway_addr:
-                        send_count(len(nn_data), push_gateway_addr)
+        d = DepthAi()
+        d.run(debug)
 
 
 def send_count(count: int, addr: str):
---
+++ people-counter-demo/src/depthai.py
@@ -0,0 +1,55 @@
+import blobconverter
+import cv2
+import depthai as dai
+from depthai_sdk import PipelineManager, NNetManager, PreviewManager
+
+# To use a different NN, change `size` and `nnPath` here:
+size = (544, 320)
+nnPath = blobconverter.from_zoo("person-detection-retail-0013", shaves=8)
+
+# Labels
+labelMap = ["background", "person"]
+
+nnSource = "color"
+
+
+class DepthAi:
+    pm: PipelineManager
+    pv: PreviewManager
+    nm: NNetManager
+
+    def __init__(self):
+        self.pm = PipelineManager()
+        self.pm.createColorCam(previewSize=size, xout=True)
+        self.pv = PreviewManager(display=["color"], nnSource=nnSource)
+
+        self.nm = NNetManager(inputSize=size, nnFamily="mobilenet", labels=labelMap, confidence=0.5)
+        nn = self.nm.createNN(self.pm.pipeline, self.pm.nodes, blobPath=nnPath, source=nnSource)
+        self.pm.setNnManager(self.nm)
+        self.pm.addNn(nn)
+
+    def run(self, debug: bool):
+        with dai.Device(self.pm.pipeline) as device:
+            self.nm.createQueues(device)
+            self.pv.createQueues(device)
+
+            while True:
+                nn_data = self.nm.decode(self.nm.outputQueue.get())
+                print(len(nn_data))
+                if debug:
+                    self._show_frame(nn_data)
+                    if self._check_quit():
+                        break
+
+    def _show_frame(self, nn_data):
+        self.pv.prepareFrames(blocking=True)
+        frame = self.pv.get("color")
+        self.nm.draw(frame, nn_data)
+        cv2.putText(frame, f"People count: {len(nn_data)}", (5, 30), cv2.FONT_HERSHEY_TRIPLEX, 1,
+                    (0, 0, 255))
+        cv2.imshow("color", frame)
+
+    def _check_quit(self) -> bool:
+        if cv2.waitKey(1) == ord('q'):
+            return True
+        return False
