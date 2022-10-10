e678e48b806ea3bdae613c5a9eaf3a9251370b3e
kmnky <cubethe27@gmail.com>
2022-10-10 18:43:57 +0900

    create simple script

---
+++ co2-sensor/main.py
@@ -0,0 +1,33 @@
+import subprocess
+import re
+import time
+
+from prometheus_client import CollectorRegistry, Gauge, push_to_gateway
+
+PUSHGATEWAY_ADDRESS = "localhost:9091"
+
+
+def main():
+    for i in range(0, 100, 1):
+        co2_conce = get_co2_conce()
+        send_co2_conce(co2_conce)
+        print(i, "分経過: ", co2_conce)
+        time.sleep(60)
+
+
+def get_co2_conce():
+    out = subprocess.check_output(["sudo", "python3", "-m", "mh_z19"]).decode("utf-8")
+    cmd_out = re.match(r"{\"co2\": (?P<conc>\d+)}", out)
+    co2_conce = int(cmd_out.group("conc"))
+    return co2_conce
+
+
+def send_co2_conce(co2_conce: int):
+    registry = CollectorRegistry()
+    g = Gauge("co2_conce", "CO2 Conce", registry=registry)
+    g.set(co2_conce)
+    push_to_gateway(PUSHGATEWAY_ADDRESS, job="co2", registry=registry)
+
+
+if __name__ == "__main__":
+    main()
